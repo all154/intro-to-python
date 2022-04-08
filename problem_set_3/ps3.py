820702e88fbc404ba7b916fb6c2613d88ea053fd
Aloisio Valério Jr <aloisio.valeriojr@gmail.com>
2022-04-08 10:43:27 -0300

    refactor(SCRABBLE_LETTER_VALUES): add value for wildcard

--- problem_set_3/ps3.py
+++ problem_set_3/ps3.py
@@ -16,7 +16,7 @@ CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
 HAND_SIZE = 7
 
 SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10, '*': 0
 }
 
 # -----------------------------------
@@ -156,8 +156,6 @@ def deal_hand(n):
         hand[x] = hand.get(x, 0) + 1
     
     return hand
-
-display_hand(deal_hand(9))
 
 #
 # Problem #2: Update a hand by removing letters
