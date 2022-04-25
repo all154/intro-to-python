822555f9951033f835290bcae7b22fb6e14dbcac
Aloisio Valério Jr <aloisio.valeriojr@gmail.com>
2022-04-25 07:30:36 -0300

    feat: get_permutations

--- problem_set_4/ps4a.py
+++ problem_set_4/ps4a.py
@@ -1,7 +1,7 @@
 # Problem Set 4A
 # Name: Aloisio Valério Jr.
 # Collaborators:
-# Time Spent: x:xx
+# Time Spent: x:xx started 9:30
 
 def get_permutations(sequence):
     '''
@@ -22,8 +22,19 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
+    permutations = []
 
-    pass #delete this line and replace with your code here
+    if len(sequence) == 1:
+        return [sequence]
+    else:
+        first_char = sequence[0]
+        permutations_of_rest = get_permutations(sequence[1:])
+
+        for word in permutations_of_rest:
+            for i in range(len(word)+1):
+                permutations.append(word[0:i] + first_char + word[i:])
+        return permutations
+
 
 if __name__ == '__main__':
 #    #EXAMPLE
@@ -36,5 +47,24 @@ if __name__ == '__main__':
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
+    example_input = 'abc'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
+
+    print('----------------')
+
+    base_case = 'a'
+    print('Input:', base_case)
+    print('Expected Output:', ['a'])
+    print('Actual Output:', get_permutations(base_case))
+
+    print('----------------')
+
+    two_letter_case = 'ab'
+    print('Input:', two_letter_case)
+    print('Expected Output:', ['ab','ba'])
+    print('Actual Output:', get_permutations(two_letter_case))
+
+    
 
