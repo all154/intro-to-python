ca96c9b998c1f191800fcae7c536dc3a675819db
Aloisio Valério Jr <aloisio.valeriojr@gmail.com>
2022-06-09 12:19:51 -0300

    feat: add OrTrigger

--- problem_set_5/ps5.py
+++ problem_set_5/ps5.py
@@ -178,9 +178,13 @@ class AndTrigger(Trigger):
         return self.trigger1.evaluate(story) and self.trigger2.evaluate(story)
 
 # Problem 9
-# TODO: OrTrigger
 class OrTrigger(Trigger):
-    pass
+    def __init__(self, trigger1, trigger2):
+        self.trigger1 = trigger1
+        self.trigger2 = trigger2
+    
+    def evaluate(self, story):
+        return self.trigger1.evaluate(story) or self.trigger2.evaluate(story)
 
 #======================
 # Filtering
